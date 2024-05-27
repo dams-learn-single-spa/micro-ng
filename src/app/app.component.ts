37cb1beb2fe8e298aa99c7e2dcb2b0ca487d3612
damar <damar@gitlab.lab>
2024-05-27 16:28:52 +0700

    implement eventBus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { singleSpaPropsSubject } from 'src/single-spa/single-spa-props';
 
 @Component({
   selector: 'micro-ng-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'micro-service-angular';
+
+  props: any;
+  constructor() {
+    singleSpaPropsSubject.subscribe((singleSpaProps) => {
+      this.props = singleSpaProps
+    })
+  }
+
+  addButton() {
+    this.props.eventBus.emit({name: 'add-count', data: true})
+  }
+
+  subButton() {
+    this.props.eventBus.emit({name: 'sub-count', data: true})
+  }
 }
